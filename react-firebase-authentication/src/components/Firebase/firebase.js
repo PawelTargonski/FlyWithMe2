a9bbcac96fe187d796c2027bc1225b647bbbf5fa
PawelTargonski <pawel.targonski@gmail.com>
2021-01-21 02:05:49 +0100

    firebase -done, with bugs

--- react-firebase-authentication/src/components/Firebase/firebase.js
+++ react-firebase-authentication/src/components/Firebase/firebase.js
@@ -1,6 +1,7 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 import app from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/database';
  
 const config = {
   apiKey: process.env.REACT_APP_API_KEY="AIzaSyAc8DjkX0m6BPuApi7sLI7SWxNaYcCvl6s",
@@ -16,6 +17,7 @@ class Firebase {
   constructor() {
     app.initializeApp(config);
     this.auth = app.auth();
+    this.db = app.database();
   }
   // *** Auth API ***
  
@@ -31,6 +33,12 @@ class Firebase {
  
     doPasswordUpdate = password =>
       this.auth.currentUser.updatePassword(password); 
+
+    // *** User API ***
+ 
+  user = uid => this.db.ref(`users/${uid}`);
+ 
+  users = () => this.db.ref('users');
 }
  
 export default Firebase;
